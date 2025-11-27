af8d6ecd776bf85855ff9bef957bd9d7ae1026fc
Pedrok45 <l202307025@vbravo.tecnm.mx>
2025-11-27 10:42:10 -0600

    Vista de surtidos, y productos funcionando

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\AbonoController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\SurtidoController;
 // Agregaremos más controladores según los vayamos creando
+// Ruta para mostrar el formulario (extraer productos)
+Route::get('/surtidos/create', [SurtidoController::class, 'create'])->name('surtidos.create');
+
+// Ruta para procesar el formulario (ejecutar el Stored Procedure)
+Route::post('/surtidos', [SurtidoController::class, 'store'])->name('surtidos.store');
+
+// Ruta opcional para listar surtidos
+Route::get('/surtidos', [SurtidoController::class, 'index'])->name('surtidos.index');
+
+
+Route::resource('productos', ProductoController::class);
+//Route::resource('surtidos', SurtidoController::class);
+
+Route::post('/surtidos/registrar', [SurtidoController::class, 'registrarConProcedimiento'])
+    ->name('surtidos.registrar');
 
 // Rutas principales
 Route::get('/', function () {
@@ -43,13 +60,13 @@ Route::get('/personas', function () {
     return view('personas.index');
 });
 
-Route::get('/productos', function () {
-    return view('productos.index');
-});
+// Route::get('/productos', function () {
+//     return view('productos.index');
+// });
 
-Route::get('/surtidos', function () {
-    return view('surtidos.index');
-});
+// Route::get('/surtidos', function () {
+//     return view('surtidos.index');
+// });
 
 Route::get('/tickets', function () {
     return view('tickets.index');
